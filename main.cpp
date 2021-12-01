2262edc90cd1da8f7665f048d163f460d387b10a
vihlancevk <vihlancevk@gmail.com>
2021-12-02 00:57:23 +0300

    Ready stack structure

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "stack.h"
+
+const char *INPUT_FILE  = "input.txt";
+const char *OUTPUT_FILE = "output.txt";
+
+int main()
+{
+    CLEAR_LOG_FILE();
+
+    stack_t stack = {};
+    int stackError = STACKCTOR_(&stack, 10);
+    IS_STACK_ERROR_(&stack, stackError);
+
+    IS_STACK_ERROR_(&stack, StackPush(&stack, 10));
+
+    IS_STACK_ERROR_(&stack, StackPush(&stack, 10));
+
+    int top = 0;
+    IS_STACK_ERROR_(&stack, StackPop(&stack, &top));
+    printf("%d\n", top);
+    IS_STACK_ERROR_(&stack, StackPop(&stack, &top));
+    printf("%d\n", top);
+
+    StackDtor(&stack);
+
+    return stackError;
+}
